8fe078a1d70f1e553ed05edc4a254336fb76e829
wuyao721 <wuyao721@163.com>
2013-07-01 19:41:17 +0800

    1

---
+++ c&c++/sizeof.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <signal.h>
+#include <stdio.h>
+#include <sys/resource.h>
+
+
+int main() {
+    printf("%d", (int) sizeof(int));
+    return 0;
+}
+
+
